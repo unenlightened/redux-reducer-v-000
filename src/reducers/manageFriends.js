9663560c0ab5991cd3132116084adafc19fc3d48
Chely Ho <adept_inept@yahoo.com>
2018-09-29 01:53:03 +0000

    automatically backed up by learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,7 +1,7 @@
 export function manageFriends(state, action){
   switch (action.type) {
     case 'ADD_FRIEND':
-      return {}
+      return {friends: [state.friends..., action.friend]}
     default:
       return state
   }
